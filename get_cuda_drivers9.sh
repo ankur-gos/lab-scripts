8b8372c751aee3069a077f5bd285e91e85ebeb48
Ankur Goswami <ankur.goswami12@gmail.com>
2018-11-18 19:58:04 -0500

    Add cuda9 drivers script - Add gitignore

---
+++ get_cuda_drivers9.sh
@@ -0,0 +1,6 @@
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+mv cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64.deb
+sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda
